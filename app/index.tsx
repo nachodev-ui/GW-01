2f0f74cbb0bab74ccaf9eda8d1d7bef28837bdc8
nachodev-ui <ig.cisternas@duocuc.cl>
2024-12-04 23:27:48 -0300

    feat: Authentication controllers in the main root file

--- app/index.tsx
+++ app/index.tsx
@@ -2,20 +2,16 @@
 import React, { useEffect, useState } from "react"
 import { Redirect } from "expo-router"
 import { onAuthStateChanged, getAuth } from "firebase/auth"
-import app from "@/firebaseConfig" // Asegúrate de que esta ruta sea correcta
+import app from "@/firebaseConfig"
 
 const Page = () => {
   const [isSignedIn, setIsSignedIn] = useState<boolean | null>(null)
 
   useEffect(() => {
-    console.log("Comprobando el estado de autenticación...")
-
     const auth = getAuth(app)
-    console.log("Firebase Auth inicializado:", auth)
-
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        console.log("Usuario autenticado:", user)
+        console.log("Usuario autenticado")
         setIsSignedIn(true)
       } else {
         console.log("No hay ningún usuario autenticado.")
@@ -24,17 +20,13 @@ const Page = () => {
     })
 
     return () => {
-      console.log("Desuscribiendo el listener de onAuthStateChanged...")
       unsubscribe()
     }
   }, [])
 
   if (isSignedIn === null) {
-    console.log("Esperando el estado de autenticación...")
-    return null // Puedes mostrar un indicador de carga mientras se verifica el estado de autenticación
+    return null
   }
-
-  console.log("Estado de isSignedIn:", isSignedIn)
 
   return isSignedIn ? (
     <Redirect href="/(root)/(tabs)/home" />
