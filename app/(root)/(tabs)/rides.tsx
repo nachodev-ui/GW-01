daffc9eeed5c26c8cdd2784b2aba66037122f8b8
nachodev-ui <ig.cisternas@duocuc.cl>
2024-12-07 12:52:37 -0300

    wip(rides): estructura inicial exploratoria, por verse

--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -1,7 +1,7 @@
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
-import RideCard from "@/components/RideCard"
+import RideCard from "@/components/OrderCard"
 import { images } from "@/constants"
 import { useFetch } from "@/lib/fetch"
 import { Ride } from "@/types/type"
